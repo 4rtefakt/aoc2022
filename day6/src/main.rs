6a0366c37b1a69eb9ebc47c6393e2d364d7acb76
4rtefakt <koreshingan@gmail.com>
2022-12-06 14:23:06 +0100

    Added day6 cleaned up.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,31 +1,55 @@
+use std::collections::VecDeque;
 use std::fs;
+
 
 fn part1() {
     println!("===== Part1 =====");
     let input = fs::read_to_string("input")
                         .expect("Should have been able to read the file");
-    let mut i = 1;
-    let mut a:char = '0';
-    let mut b:char = '0';
-    let mut c:char = '0';
+
+    let mut i = 0;
+    let mut q: VecDeque<char> = VecDeque::new();
+
     for ch in input.chars() {
-        if a != ch && b != ch && c != ch && i > 3 && a != '0' && a != b && a != c && b != c {
-            println!("Need to process {} characters.", i);
+        q.push_back(ch);
+        i += 1;
+        if q.len() > 4 {
+            q.pop_front();
+        }
+        let mut valid = true;
+        for item in &q {
+            valid = q.len() == 4 && valid && q.iter().filter(|&n| *n == *item).count() < 2;
+        }
+        if valid { 
+            println!("Solution is {}.", i);
             break;
         }
-        a = b;
-        b = c;
-        c = ch;
-        i += 1;
     }
-
 }
 
 fn part2() {
         println!("===== Part2 =====");
-        let _input = fs::read_to_string("input")
+        let input = fs::read_to_string("input")
                             .expect("Should have been able to read the file");
+        
+        let mut i = 0;
+        let mut q: VecDeque<char> = VecDeque::new();
 
+        for ch in input.chars() {
+            q.push_back(ch);
+            i += 1;
+            if q.len() > 14 {
+                q.pop_front();
+            }
+            let mut valid = true;
+            for item in &q {
+                valid = q.len() == 14 && valid && q.iter().filter(|&n| *n == *item).count() < 2;
+            }
+            if valid { 
+                println!("Solution is {}.", i);
+                break;
+            }
+        }
 }
 
 fn main () {
